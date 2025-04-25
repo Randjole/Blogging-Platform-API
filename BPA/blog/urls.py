4f47cd4901941efb79370f3b8c1a9cc7548a4de3
Randjole <randjole15@gmail.com>
2025-04-25 17:38:00 +0200

    'again'

--- BPA/blog/urls.py
+++ BPA/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('blog',views.blogging_platfrom, name ='blogging-page'),
-    path('blog',views.posts, name = 'posts-page')
+    path('blog',views.posts, name = 'posts-page'),
+
 ]
--- BPA/blog/views.py
+++ BPA/blog/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
-from .models import back_name
 
 
-all_posts = {
+
+all_posts = [{
     
   "title": "My First Blog Post",
   "content": "This is the content of my first blog post.",
   "category": "Technology",
-  "tags": ["Tech", "Programming"]
-}
+  "tags": ["Tech", "Programming"],
+  "date": date(2025,1,1),
+}]
 
 
 
+
+def get_date(post):
+    return post['date']
 
 def blogging_platfrom(request):
-    return render(request, 'html/index.html',{
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'html/index.html',{'posts':latest_posts})
 
-    })
 def posts(request):
     return render(request, 'blog/all-posts.html',{'all_posts':all_posts})  
 
